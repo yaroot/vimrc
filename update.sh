9be115da08db11816cf7cc20f9d0d681598fc49a
yaroot <yaroot@gmail.com>
2013-04-23 13:12:36 +0800

    .

--- update.sh
+++ update.sh
@@ -13,5 +13,8 @@ VIMD="$HOME/.vim"
 
 mkdir -p $VIMD/{indent,syntax}
 
+# http://www.vim.org/scripts/script.php?script_id=790
+_install "http://www.vim.org/scripts/download_script.php?src_id=19619" $VIMD/syntax/python.vim
+
 # protobuf
 _install "http://protobuf.googlecode.com/svn/trunk/editors/proto.vim" $VIMD/syntax/proto.vim
