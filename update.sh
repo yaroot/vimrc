b113364b9a08404d753bd3829bd45c8ea6750536
yaroot <yaroot@yaroot.net>
2014-01-04 08:33:48 +0800

    .

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-_install()
+unterladen()
 {
   local url="$1"
   local file="$2"
@@ -14,7 +14,7 @@ VIMD="$HOME/.vim"
 mkdir -p $VIMD/{indent,syntax}
 
 # http://www.vim.org/scripts/script.php?script_id=790
-_install "http://www.vim.org/scripts/download_script.php?src_id=19619" $VIMD/syntax/python.vim
+unterladen "http://www.vim.org/scripts/download_script.php?src_id=19619" $VIMD/syntax/python.vim
 
 # protobuf
-_install "http://protobuf.googlecode.com/svn/trunk/editors/proto.vim" $VIMD/syntax/proto.vim
+unterladen "http://protobuf.googlecode.com/svn/trunk/editors/proto.vim" $VIMD/syntax/proto.vim
