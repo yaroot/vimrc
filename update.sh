27d0f8ecb730e9af07681abadb85c6531ca5c36c
tsu <tsu@yaroot.net>
2016-06-20 15:19:05 +0800

    .

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ mkdir -p $VIMD/{indent,syntax}
 # http://www.vim.org/scripts/script.php?script_id=790
 unterladen 'http://www.vim.org/scripts/download_script.php?src_id=21056'    $VIMD/syntax/python.vim
 
-unterladen 'http://protobuf.googlecode.com/svn/trunk/editors/proto.vim'     $VIMD/syntax/proto.vim
+unterladen 'https://github.com/google/protobuf/raw/master/editors/proto.vim'     $VIMD/syntax/proto.vim
 unterladen 'http://haproxy.1wt.eu/download/contrib/haproxy.vim'             $VIMD/syntax/haproxy.vim
 unterladen 'http://svn.apache.org/repos/asf/thrift/attic/trunk/contrib/thrift.vim'  $VIMD/syntax/thrift.vim
 unterladen 'http://www.complang.org/ragel/ragel.vim'                        $VIMD/syntax/ragel.vim
