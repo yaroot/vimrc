9910c8117ca43c1774f3f1f8d19e4b14e52b9322
tsu <tsu@yaroot.net>
2016-07-26 08:56:16 +0000

    .

--- update.sh
+++ update.sh
@@ -15,8 +15,7 @@ mkdir -p $VIMD/{indent,syntax}
 
 # http://www.vim.org/scripts/script.php?script_id=790
 unterladen 'http://www.vim.org/scripts/download_script.php?src_id=21056'    $VIMD/syntax/python.vim
-
-unterladen 'https://github.com/google/protobuf/raw/master/editors/proto.vim'     $VIMD/syntax/proto.vim
+unterladen 'https://github.com/google/protobuf/raw/master/editors/proto.vim'        $VIMD/syntax/proto.vim
 unterladen 'http://haproxy.1wt.eu/download/contrib/haproxy.vim'             $VIMD/syntax/haproxy.vim
 unterladen 'http://svn.apache.org/repos/asf/thrift/attic/trunk/contrib/thrift.vim'  $VIMD/syntax/thrift.vim
 
